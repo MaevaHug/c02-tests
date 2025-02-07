b2826f1c6723a2e2fc9e1faaf29dc5b5b58411ab
Maeva Hug <maevahug@outlook.fr>
2025-02-07 06:23:09 +0100

    ex06 usage more explicit

--- ex06/main.c
+++ ex06/main.c
@@ -6,7 +6,7 @@
 /*   By: mahug <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 22:01:15 by mahug             #+#    #+#             */
-/*   Updated: 2025/02/01 05:20:21 by mahug            ###   ########.fr       */
+/*   Updated: 2025/02/07 06:21:58 by mahug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Usage: %s <string>\n", argv[0]);
+		printf("Examples:\n");
+		printf("  %s \"Hello\"             # normal string\n", argv[0]);
+		printf("  %s $'Hello\\n'          # with newline\n", argv[0]);
+		printf("  %s $'Hello\\t'          # with tab\n", argv[0]);
+		printf("  %s $'\\x00'             # null char\n", argv[0]);
+		printf("  %s $'Hello\\x7FWorld'   # with DEL char\n", argv[0]);
 		return (0);
 	}
 	printf("%d\n", ft_str_is_printable(argv[1]));
